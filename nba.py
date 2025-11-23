f28290df23cf9abe6b4369b6807f92c7a9b3128d
kevinkairouz <KM.Kairouz@gmail.com>
2025-11-23 02:02:01 -0500

    added search functions

--- nba.py
+++ nba.py
@@ -1,19 +1,57 @@
 import pandas as pd   
 import matplotlib as plt 
 
-dataSet = pd.read_csv("nbaNew.csv")  
+def searchPlayerStats(playerName): 
+
+    dataSet = pd.read_csv("nbaNew.csv")    
+    d = dataSet.groupby("PlayerName")[["PTS", "G", "AST", "TRB"]].sum()   
+
+    d["PPG"] = d["PTS"]/d["G"]  
+    d["RPG"] = d["TRB"]/d["G"]  
+    d["APG"] = d["AST"]/d["G"]  
+
+    print(d.loc[playerName][["PPG", "RPG", "APG"]]) 
+    print(f"{searchPlayer} has played for the following") 
+    teamQuery = dataSet[dataSet["PlayerName"] == playerName]  
+    print(teamQuery[["Tm"]].drop_duplicates())  
+    return 
+
+def findTopFive():  
+    dataSet = pd.read_csv("nbaNew.csv")    
+    d = dataSet.groupby("PlayerName")[["PTS", "G", "AST", "TRB"]].sum()   
+
+    d["PPG"] = d["PTS"]/d["G"]  
+    d["RPG"] = d["TRB"]/d["G"]  
+    d["APG"] = d["AST"]/d["G"]  
+
+    d = d.sort_values(by=["PPG", "RPG", "APG"], ascending= False)    
+    print(d.head()) 
+
+def findWorstFive(): 
+    dataSet = pd.read_csv("nbaNew.csv")    
+    d = dataSet.groupby("PlayerName")[["PTS", "G", "AST", "TRB"]].sum()   
+
+    d["PPG"] = d["PTS"]/d["G"]  
+    d["RPG"] = d["TRB"]/d["G"]  
+    d["APG"] = d["AST"]/d["G"]  
+
+    d = d.sort_values(by=["PPG", "RPG", "APG"], ascending= False)    
+    print(d.tail())
 
 
-d = dataSet.groupby("PlayerName")[["PTS", "G", "AST", "TRB"]].sum()   
-
-d["PPG"] = d["PTS"]/d["G"]  
-d["RPG"] = d["TRB"]/d["G"]  
-d["APG"] = d["AST"]/d["G"]
 
 
-d = d.sort_values(by=["PPG"], ascending= False)   
+print("Welcome to the NBA player finder")
+searchPlayer = str(input("Enter an NBA player: "))  
 
-print(d[["PPG", "APG", "RPG"]].head())
+searchPlayerStats(searchPlayer)
+
+
+
+
+
+
+
 
 
 
